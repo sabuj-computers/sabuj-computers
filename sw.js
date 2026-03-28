217fb8bdf00eafee58f2c0b3b13604da75e06340
sabuj-computers <akkhatun005@gmail.com>
2026-03-28 18:35:32 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+﻿const cacheName = 'sabuj-comp-v1';
+const assets = [
+  '/sabuj-computers/',
+  '/sabuj-computers/index.html',
+  '/sabuj-computers/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
+});
